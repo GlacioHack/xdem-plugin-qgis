4a54f97e14ebf037ca79d78e0d9ab6c881bb4cd0
ould-a <Antoine.OuldElhocineTaiariol@cnes.fr>
2026-05-19 10:27:16 +0200

    Added a save for test examples

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import os
 import xdem
 import pytest
 from qgis.core import QgsRasterLayer
@@ -5,13 +6,17 @@ from qgis.core import QgsRasterLayer
 
 @pytest.fixture
 def ref_dem_layer():
-    ref_dem_path = xdem.examples.get_path("longyearbyen_ref_dem")
+    ref_dem_path = xdem.examples.get_path(
+        "longyearbyen_ref_dem", output_dir=os.path.dirname(__file__)
+    )
     layer = QgsRasterLayer(ref_dem_path)
     return layer
 
 
 @pytest.fixture
 def tba_dem_layer():
-    tba_dem_path = xdem.examples.get_path("longyearbyen_tba_dem")
+    tba_dem_path = xdem.examples.get_path(
+        "longyearbyen_tba_dem", output_dir=os.path.dirname(__file__)
+    )
     layer = QgsRasterLayer(tba_dem_path)
     return layer
