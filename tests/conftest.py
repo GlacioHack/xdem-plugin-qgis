f1d7597d852ca5166107873b7184ece0b189c164
ould-a <Antoine.OuldElhocineTaiariol@cnes.fr>
2026-05-22 13:36:17 +0200

    Changes to the test examples

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,22 +1,17 @@
-import os
-import xdem
 import pytest
+import xdem
 from qgis.core import QgsRasterLayer
 
 
 @pytest.fixture
 def ref_dem_layer():
-    ref_dem_path = xdem.examples.get_path(
-        "longyearbyen_ref_dem", output_dir=os.path.dirname(__file__)
-    )
+    ref_dem_path = xdem.examples.get_path("longyearbyen_ref_dem")
     layer = QgsRasterLayer(ref_dem_path)
     return layer
 
 
 @pytest.fixture
 def tba_dem_layer():
-    tba_dem_path = xdem.examples.get_path(
-        "longyearbyen_tba_dem", output_dir=os.path.dirname(__file__)
-    )
+    tba_dem_path = xdem.examples.get_path("longyearbyen_tba_dem")
     layer = QgsRasterLayer(tba_dem_path)
     return layer
