a458bc8db78b903720274bbda758a5d181088a47
ould-a <Antoine.OuldElhocineTaiariol@cnes.fr>
2026-05-27 10:08:44 +0200

    Simplification of the log function

--- xdem_installer.py
+++ xdem_installer.py
@@ -57,17 +57,16 @@ class XdemInstaller:
             "shapely",
         ]
 
-    def info_log(self, message):
+    def log(self, message, error=True):
         """
         Displays information in the QGIS console, in the xDEM section.
         """
-        QgsMessageLog.logMessage(message, tag="xDEM", level=Qgis.MessageLevel.Info)
+        if error:
+            level = Qgis.MessageLevel.Critical
+        else:
+            level = Qgis.MessageLevel.Info
 
-    def error_log(self, message):
-        """
-        Displays error in the QGIS console, in the xDEM section.
-        """
-        QgsMessageLog.logMessage(message, tag="xDEM", level=Qgis.MessageLevel.Critical)
+        QgsMessageLog.logMessage(message, "xDEM", level)
 
     def exist_in_qgis(self, package):
         """
@@ -125,7 +124,7 @@ class XdemInstaller:
         """
         # Python check, xdem works starting with version 3.10
         if sys.version_info < (3, 10):
-            self.error_log("Installation failled, python version lower than 3.10")
+            self.log("Installation failled, python version lower than 3.10")
             return False
 
         # Installing packages and managing conflicts
@@ -136,7 +135,7 @@ class XdemInstaller:
                 self.clean_shared_packages()
             except Exception as e:
                 shutil.rmtree(self.libs_folder)
-                self.error_log(f"Unable to install xdem, error:{e}")
+                self.log(f"Unable to install xdem, error:{e}")
                 return False
 
         # Add libs folder to the python path as the first entry
@@ -145,13 +144,13 @@ class XdemInstaller:
 
         if not self.exist_in_qgis("xdem"):
             shutil.rmtree(self.libs_folder)
-            self.error_log("Unable to import xdem after installation")
+            self.log("Unable to import xdem after installation")
             return False
 
         if not self.check_dependencies():
             shutil.rmtree(self.libs_folder)
-            self.error_log("Unable to install xdem, requirements are not satisfie")
+            self.log("Unable to install xdem, requirements are not satisfie")
             return False
 
-        self.info_log("xdem succesfully loaded")
+        self.log("xdem succesfully loaded", error=False)
         return True
