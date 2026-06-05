e1bf9f644b629e7a04bc0c6dd7b9ea5c73b42a15
ould-a <ouldantoine@gmail.com>
2026-06-05 09:24:07 +0200

    Remove pdf build in workflows

--- algorithms/workflows.py
+++ algorithms/workflows.py
@@ -35,17 +35,6 @@ from .processing_tools import XdemProcessingAlgorithm
 COREG_METHODS = COREG_METHODS[:-1]  # Squeeze the last value (None)
 
 
-def generate_pdf(workflow, feedback):
-    try:
-        from weasyprint import HTML
-
-        HTML(workflow.outputs_folder / "report.html").write_pdf(
-            workflow.outputs_folder / "report.pdf"
-        )
-    except Exception as e:
-        feedback.pushWarning(f"Unable to generate pdf, error: {e}")
-
-
 class AccuracyWorkflow(XdemProcessingAlgorithm):
     """
     This class is designed to perform an accuracy assessment of an elevation dataset.
@@ -178,7 +167,6 @@ class AccuracyWorkflow(XdemProcessingAlgorithm):
 
         workflow = Accuracy(config)
         workflow.run()
-        generate_pdf(workflow, feedback)
 
         rasters_folder = os.path.join(output_folder, "rasters")
         for file in os.listdir(rasters_folder):
@@ -297,7 +285,6 @@ class TopoWorkflow(XdemProcessingAlgorithm):
 
         workflow = Topo(config)
         workflow.run()
-        generate_pdf(workflow, feedback)
 
         rasters_folder = os.path.join(output_folder, "rasters")
         for file in os.listdir(rasters_folder):
